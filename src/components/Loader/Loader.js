da66c686965eb9f976341bf302e40075ca6a3bab
Akshay00003 <akshayprakash4003@gmail.com>
2024-02-01 19:58:27 +0530

    Done

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loader.css'
+function Loader() {
+  return (
+   <div style={{display:"flex", justifyContent:"center"}}>
+    <span class="loader"></span>
+   </div>
+  )
+}
+
+export default Loader
